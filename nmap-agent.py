cb3305003b2a5449f647a6a362ac746c6cddb113
pierce403 <pierce403@gmail.com>
2016-05-20 14:13:09 -0700

    Update nmap-agent.py
    Check private addresses, and while I'm at it, port to python3

--- nmap-agent.py
+++ nmap-agent.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 import sys
-import urllib2
+import requests
 import subprocess
 import time
 
@@ -7,12 +9,22 @@ import threading
 import multiprocessing
 import multiprocessing.pool
 
+try:
+  import ipaddress
+except:
+  print("you should be using python3!")
+  sys.exit()
+
 def scan():
   # pull target
-  server="http://127.0.0.1:8000"
-  #server="https://nweb.io"
-  target = urllib2.urlopen(server+"/getwork").read()
+  #server="http://127.0.0.1:8000"
+  server="https://nweb.io"
+  target = requests.get(server+"/getwork").text
   print("target is "+target)
+
+  if ipaddress.ip_address(target).is_private:
+    print("no I'm not going to scan a private ip.. "+target)
+    return
 
   # scan server 
   process = subprocess.Popen(["nmap","-A","-open",target],stdout=subprocess.PIPE)
@@ -21,22 +33,20 @@ def scan():
   #print(out)
 
   # submit result
-  response=urllib2.urlopen(server+"/submit",data=out).read()
+  response=requests.post(server+"/submit",data=out).text
   print("response is:\n"+response)
 
-max_threads=30
+max_threads=10
 
 while True:
   if threading.active_count() < max_threads:
     notifylock=False
-    print "number of threads : "+str(threading.active_count())
+    print("number of threads : "+str(threading.active_count()))
     t = threading.Thread(target=scan)
     t.start()
   else:
     if notifylock is False:
-      print "too many threads .. waiting"
+      print("too many threads .. waiting")
     notifylock=True
-  
+
   time.sleep(1)
-
-
