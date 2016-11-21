1bb76c96edd4595da93746bb866ccd68f2db7723
Michael Leibowitz <michael.leibowitz@intel.com>
2016-11-21 11:47:24 -0800

    Change to venv
    Now automagically runs with virtualenv and python3.
    
    Signed-off-by: Michael Leibowitz <michael.leibowitz@intel.com>

--- run-flask.sh
+++ run-flask.sh
@@ -1,8 +1,17 @@
-export FLASK_APP=main.py
+export FLASK_APP=./main.py
+
+if [ ! -d venv ]
+then
+  virtualenv -p /usr/bin/python3 venv
+fi
+
+source venv/bin/activate
+pip3 install flask
+pip3 install netaddr
 
 while [ 1 == 1 ]
 do
   echo `date` >> start.log
-  python3 -m flask run # --host=0.0.0.0
+  flask run # --host=0.0.0.0
   sleep 5
 done
