1a593fe4699224993c93ba541ded49bca6e6c749
Dean Pierce <pierce403@gmail.com>
2016-11-21 17:16:08 -0800

    cleaning up the django mess

--- fix-index.sh
+++
@@ -1,1 +0,0 @@
-mysql -unweb -pnweb --database nweb -e ALTER TABLE scan_manager_host ADD FULLTEXT index_name(data);
--- run.sh
+++
@@ -1,3 +0,0 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
