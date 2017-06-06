8a46438b9cb7eb00b4af9fae662a7a35ae053aee
Dean Pierce <pierce403@gmail.com>
2017-06-05 18:49:40 -0700

    new code for elastic search interaction

---
+++ models_elastic.py
@@ -0,0 +1,28 @@
+import json
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+def search(query,limit,offset):
+
+    if query=='':
+        query='nmap'
+
+    print("query is %s",query)
+
+    result = es.search(index="nweb", doc_type="nmap", body={"size":limit, "from":offset, "query": {"match": {'nmap_data':'nmap'}}})
+    count = 1
+
+    results=[] # collate results
+    for thing in result['hits']['hits']:
+        results.append(thing['_source'])
+
+    return result['hits']['total'],results
+
+def newhost(host):
+    ip = str(host['ip'])
+    es.index(index='nweb', doc_type='nmap_history', body=host)
+    es.index(index='nweb', doc_type='nmap', id=ip, body=host)
+
+def gethost(ip):
+    result = es.get(index='nweb', doc_type='nmap', id=ip)
+    return result['_source']
