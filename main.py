b109ba29fca3f50e53580280063c75605484b638
pierce403 <pierce403@gmail.com>
2017-09-06 11:12:11 -0700

    removing some django junk

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-#from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.views.decorators.http import require_http_methods
-#from django.views.decorators.csrf import csrf_exempt
-
-#from scan_manager.models import Host
-
 import flask
 from flask import render_template
 from flask import request
