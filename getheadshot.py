8e35905a18864e42e9afffee4a2bb84780c64274
Dean Pierce <pierce403@gmail.com>
2017-09-06 18:19:44 -0700

    add vnc support

--- getheadshot.py
+++ getheadshot.py
@@ -2,8 +2,9 @@
 
 import subprocess
 
-# wkhtmltoimage --width 50 --quality 50 <target> out.png
-
+# apt-get install wkhtmltopdf vncsnapshot
+# wkhtmltoimage --width 50 --quality 50 <target> out.out
+# vncsnapshot -quality 50 <target> out.jpg
 
 def getheadshot(ip,rand):
 
@@ -15,6 +16,11 @@ def getheadshot(ip,rand):
   process = subprocess.Popen(["wkhtmltoimage","--width","50","--quality","80","-f","jpg","https://"+ip,"data/nweb."+rand+".headshot.jpg"],stdout=subprocess.PIPE)
   if process.wait() is 0:
     print("seems like https worked")
+    return True
+
+  process = subprocess.Popen(["vncsnapshot","-quality","50",ip,"data/nweb."+rand+".headshot.jpg"],stdout=subprocess.PIPE)
+  if process.wait() is 0:
+    print("seems like vnc worked")
     return True
 
   print("seems like nothing worked")
