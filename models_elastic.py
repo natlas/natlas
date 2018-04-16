17ce3017614358744d557e2aeb06a20c2c83ddb3
Dean Pierce <pierce403@gmail.com>
2018-04-16 16:20:28 -0700

    two is the correct number

--- models_elastic.py
+++ models_elastic.py
@@ -4,31 +4,31 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 def search(query,limit,offset):
 
-    if query=='':
-        query='nmap'
+  if query=='':
+    query='nmap'
 
-    print("query is %s",query)
+  print("query is %s",query)
 
-    try:
-      result = es.search(index="nmap", doc_type="_doc", body={"size":limit, "from":offset, "query": {"query_string": { 'query':query, "fields":["nmap_data"], "default_operator":"AND"  } },"sort": { "ctime": { "order": "desc" }}})
-    except:
-      return 0,[] # search borked, return nothing
+  try:
+    result = es.search(index="nmap", doc_type="_doc", body={"size":limit, "from":offset, "query": {"query_string": { 'query':query, "fields":["nmap_data"], "default_operator":"AND"  } },"sort": { "ctime": { "order": "desc" }}})
+  except:
+    return 0,[] # search borked, return nothing
 
-    #result = es.search(index="nmap", doc_type="_doc", body={"size":limit, "from":offset, "query": {"match": {'nmap_data':query}}})
-    count = 1
+  #result = es.search(index="nmap", doc_type="_doc", body={"size":limit, "from":offset, "query": {"match": {'nmap_data':query}}})
+  count = 1
 
-    results=[] # collate results
-    for thing in result['hits']['hits']:
-        results.append(thing['_source'])
+  results=[] # collate results
+  for thing in result['hits']['hits']:
+    results.append(thing['_source'])
 
-    return result['hits']['total'],results
+  return result['hits']['total'],results
 
 def newhost(host):
-    ip = str(host['ip'])
-    # broken in ES6
-    es.index(index='nmap_history', doc_type='_doc', body=host)
-    es.index(index='nmap', doc_type='_doc', id=ip, body=host)
+  ip = str(host['ip'])
+  # broken in ES6
+  es.index(index='nmap_history', doc_type='_doc', body=host)
+  es.index(index='nmap', doc_type='_doc', id=ip, body=host)
 
 def gethost(ip):
-    result = es.get(index='nmap', doc_type='_doc', id=ip)
-    return result['_source']
+  result = es.get(index='nmap', doc_type='_doc', id=ip)
+  return result['_source']
