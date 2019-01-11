eea6f765ca61f11ed7acaf7c2a115ef099c20782
0xdade <0xdade@users.noreply.github.com>
2019-01-11 00:17:30 -0800

    Improve add-admin, allow it to create new admin users with random passwords
    Added a shebang for python3, script can now be used to seed an initial admin user into the database with a randomly generated password.

--- natlas-server/add-admin.py
+++ natlas-server/add-admin.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3
 import argparse
+import random
+import string
 from app import app, db
 from app.models import User
+
+pass_length = 16
 
 parser = argparse.ArgumentParser()
 parser.add_argument("email")
 args = parser.parse_args()
 user = User.query.filter_by(email=args.email).first()
-user.is_admin = True
-db.session.add(user)
-db.session.commit()
+if user is not None:
+	user.is_admin = True
+	db.session.add(user)
+	db.session.commit()
+	print("User %s is now an admin" % args.email)
+else:
+	password=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(pass_length))
+	user = User(email=args.email, is_admin=True)
+	user.set_password(password)
+	db.session.add(user)
+	db.session.commit()
+	print("User %s has been created as an admin with password %s" % (args.email, password))
