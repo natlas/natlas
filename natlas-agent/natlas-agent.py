2d004dc1fcb6547d5fabaacca32a6bad312256e5
0xdade <0xdade@users.noreply.github.com>
2019-01-11 03:12:20 -0800

    Massive improvements to the agent
    Implemented multiple modes for the agent to run in, including single-target, cidr-range, target-file, and range-file. Also implemented better threading and a queue in order to support these different operational modes. Also implemented agent-side check for the nmap scan report header in order to save bandwidth.

--- natlas-agent/natlas-agent.py
+++ natlas-agent/natlas-agent.py
@@ -9,29 +9,27 @@ import random
 import string
 import json
 import base64
+import argparse
 
 import threading
-import multiprocessing
-import multiprocessing.pool
+import queue
 
 # my script for headshotting servers
 from getheadshot import getheadshot
 from config import Config
 
-try:
-    import ipaddress
-except:
-    print("[!] ipaddress module not found")
-    sys.exit()
+import ipaddress
 
 config = Config()
+MAX_QUEUE_SIZE = 100 # setting max queue size to 100 so that we can take advantage of the memory benefits of using the .hosts() iterator instead of loading all hosts into a queue
 
-
-def scan():
+def scan(target=None):
     server = config.server
-    print("[+] Fetching Target from %s" % server)
-    target_data = json.loads(requests.get(server+"/getwork").text)
-    target = target_data["target"]
+    if target is None:
+        print("[+] Fetching Target from %s" % server)
+        target_data = json.loads(requests.get(server+"/getwork").text)
+        target = target_data["target"]
+
     print("[+] Target: "+target)
 
     # scan server
@@ -40,10 +38,6 @@ def scan():
     print("[+] Scan ID: "+rand)
 
     command = ["nmap", "-oA", "data/natlas."+rand, "-sV", "-O","-sC", "-open", target]
-
-    if 'ports' in target_data:
-        command.append('-p')
-        command.append(str(target_data['ports'])[1:-1])
 
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     try:
@@ -67,6 +61,9 @@ def scan():
 
     if len(result['nmap_data']) < 250:
         print("[!] (%s) Nmap data is too short" % rand)
+        return
+    elif 'Nmap scan report for' not in result['nmap_data']: # checking for this on the agent saves bandwidth
+        print("[!] (%s) Nmap scan report not found" % rand)
         return
     else:
         print("[+] (%s) scan size: %s" % (rand, len(result['nmap_data'])))
@@ -101,22 +98,104 @@ def scan():
     response = requests.post(server+"/submit", json=json.dumps(result)).text
     print("[+] (%s) Response:\n%s" % (rand, response))
 
+class ThreadScan(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            target = self.queue.get()
+            scan(target)
+            self.queue.task_done()
 
 def main():
     if not os.path.isdir("data"):
         os.mkdir("data")
-    while True:
-        if threading.active_count() <= config.max_threads:
-            notifylock = False
-            print("[+] Active Threads: %s" % threading.active_count())
-            t = threading.Thread(target=scan)
-            t.start()
-        else:
-            if notifylock is False:
-                print("[+] Thread pool exhausted")
-            notifylock = True
 
-        time.sleep(1)
+    PARSER_DESC = "Scan hosts and report data to a configured server. The server will reject your findings if they are deemed not in scope."
+    PARSER_EPILOG = "Report problems to https://github.com/natlas/natlas"
+    parser = argparse.ArgumentParser(description=PARSER_DESC, epilog=PARSER_EPILOG)
+    mutually_exclusive = parser.add_mutually_exclusive_group()
+    mutually_exclusive.add_argument('--target', metavar='IPADDR', help="An IPv4 address to scan. e.g. 192.168.0.1", dest='target')
+    mutually_exclusive.add_argument('--range', metavar='CIDR', help="A CIDR block to scan. e.g. 192.168.0.0/24", dest='range')
+    mutually_exclusive.add_argument('--target-file', metavar='FILENAME', help="A file of line separated target IPv4 addresses", dest='tfile')
+    mutually_exclusive.add_argument('--range-file', metavar='FILENAME', help="A file of line separated target CIDR blocks", dest='rfile')
+    args = parser.parse_args()
+    
+    if args.target:
+        print("[+] Running in single-target mode")
+        scan(args.target)
+        
+        print("[+] Finished scanning the single target: %s" % args.target)
+        return
+
+    elif args.range:
+        print("[+] Running in target-range mode\n[+] Target Range: %s" % args.range)
+        targetNetwork = ipaddress.ip_network(args.range, False)
+        q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+
+        # Start threads that will wait for items in queue and then scan them
+        for i in range(config.max_threads):
+            t = ThreadScan(q)
+            t.setDaemon(True)
+            t.start()
+
+        # Iterate over items in target, queue.put will block until a queue slot is available
+        for target in targetNetwork.hosts(): 
+            q.put(str(target))
+
+        # queue.join to wait until the queue is processed once we've finished stuffing hosts into the queue
+        q.join()
+        print("[+] Finished scanning the target range %s" % args.range)
+        return
+
+    elif args.tfile:
+        print("[+] Running in target-file mode\n[+] Target File: %s" % args.tfile)
+        q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+
+        for i in range(config.max_threads):
+            t = ThreadScan(q)
+            t.setDaemon(True)
+            t.start()
+
+        for target in open(args.tfile, "r"):
+            q.put(str(target.strip()))
+        
+        q.join()
+        print("[+] Finished scanning the target file %s" % args.tfile)
+        return
+
+    elif args.rfile:
+        print("[+] Running in range-file mode\n[+] Range File: %s" % args.rfile)
+        q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+
+        for i in range(config.max_threads):
+            t = ThreadScan(q)
+            t.setDaemon(True)
+            t.start()
+
+        for trange in open(args.rfile, "r"):
+            targetNetwork = ipaddress.ip_network(trange.strip())
+            for target in targetNetwork.hosts():
+                q.put(str(target))
+
+        q.join()
+        print("[+] Finished scanning the range file %s" % args.rfile)
+        return
+    # This is the default behavior of fetching work from the server
+    else:
+        while True:
+            if threading.active_count() <= config.max_threads:
+                notifylock = False
+                print("[+] Active Threads: %s" % threading.active_count())
+                t = threading.Thread(target=scan)
+                t.start()
+            else:
+                if notifylock is False:
+                    print("[+] Thread pool exhausted")
+                notifylock = True
+            time.sleep(1) 
 
 
 if __name__ == "__main__":
--- tools/elastic-bumpmaxresults.sh
+++
@@ -1,4 +0,0 @@
-# run this if the default result window of 10,000 results
-# is too low (maximum number of returned search results)
-
-curl -XPUT "http://localhost:9200/nweb/_settings" -d '{ "max_result_window" : 500000 }';
