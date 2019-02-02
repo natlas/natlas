eb1376e9ecbde4ede7995ab555d7cb8f2f13405a
0xdade <0xdade@users.noreply.github.com>
2019-02-01 23:26:18 -0800

    Add additional comments to describe agent config options

--- natlas-agent/config.py
+++ natlas-agent/config.py
@@ -3,10 +3,10 @@ import os
 class Config:
 
     def __init__(self):
-        self.server = os.environ.get('NATLAS_SERVER_ADDRESS') or 'http://127.0.0.1:5000'
-        self.max_threads = os.environ.get('NATLAS_MAX_THREADS') or 3
+        self.server = os.environ.get('NATLAS_SERVER_ADDRESS') or 'http://127.0.0.1:5000' # url of server to get/submit work to
+        self.max_threads = os.environ.get('NATLAS_MAX_THREADS') or 3 # maximum number of threads to utilize
         self.scan_timeout = os.environ.get('NATLAS_SCAN_TIMEOUT') or 360 # seconds, 6 minutes default
-        self.scan_local = os.environ.get('NATLAS_SCAN_LOCAL') or False
+        self.scan_local = os.environ.get('NATLAS_SCAN_LOCAL') or False # Are we allowed to scan local addresses?
         self.request_timeout = os.environ.get('NATLAS_REQUEST_TIMEOUT') or 15 # seconds
         self.backoff_max = os.environ.get('NATLAS_BACKOFF_MAX') or 300 # seconds, 5 minutes default
         self.backoff_base = os.environ.get('NATLAS_BACKOFF_BASE') or 1 # seconds
