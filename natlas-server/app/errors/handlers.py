2a2c954d57e2f82a38d4bf263454dfe0282fe68c
0xdade <0xdade@users.noreply.github.com>
2019-02-04 00:40:05 -0800

    Implement custom error handler for 405 Method Not Allowed

--- natlas-server/app/errors/handlers.py
+++ natlas-server/app/errors/handlers.py
@@ -6,6 +6,9 @@ from app.errors import bp
 def page_not_found(e):
     return render_template('errors/404.html'), 404
 
+@bp.app_errorhandler(405)
+def internal_server_error(e):
+    return render_template('errors/405.html'), 405
 
 @bp.app_errorhandler(500)
 def internal_server_error(e):
