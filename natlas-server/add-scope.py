2cacc857dedcba2d990cdcb9cc064ec15a4bc2a7
0xdade <0xdade@users.noreply.github.com>
2019-02-03 22:44:34 -0800

    Add verbosity to add-scope script
    Add a verbosity flag that prints out each scope from the file and whether it succeeded, failed, or already existed. Closes #58. Also mask out host bits in any provided scopes.

--- natlas-server/add-scope.py
+++ natlas-server/add-scope.py
@@ -7,41 +7,56 @@ from app.models import ScopeItem
 app = create_app()
 app.app_context().push()
 
-def importScope(file, blacklist):
-    successImport = 0
-    alreadyExists = 0
+def importScope(file, blacklist, verbose):
     failedImports = []
+    alreadyExists = []
+    successImports = []
     with open(file, 'r') as scope:
         for line in scope.readlines():
             line = line.strip()
             if '/' not in line:
                 line = line + '/32'
             try:
-                isValid = ipaddress.IPv4Network(line)
+                isValid = ipaddress.ip_network(line, False) # False will mask out hostbits for us, ip_network for eventual ipv6 compat
             except ValueError as e:
-                failedImports.append(e)
+                failedImports.append(line) # if we hit this ValueError it means that the input couldn't be a CIDR range
                 continue
-            item = ScopeItem.query.filter_by(target=line).first()
+            item = ScopeItem.query.filter_by(target=isValid.with_prefixlen).first() # We only want scope items with masked out host bits
             if item:
-                alreadyExists += 1
+                alreadyExists.append(isValid.with_prefixlen)
                 continue
             else:
-                newTarget = ScopeItem(target=line, blacklist=blacklist)
+                newTarget = ScopeItem(target=isValid.with_prefixlen, blacklist=blacklist)
                 db.session.add(newTarget)
-                db.session.commit()
-                successImport += 1
-    print("%s\n-----\n%s successfully imported.\n%s already existed.\nFailed Imports:" %
-          (file, successImport, alreadyExists))
-    for i in failedImports:
-        print("[!] %s" % i)
-    print("\n")
+                successImports.append(isValid.with_prefixlen)
+    db.session.commit()
+    print("%s successfully imported.\n%s already existed.\n%s failed to import." %
+          (len(successImports), len(alreadyExists), len(failedImports)))
+    if verbose:
+        print("\nSuccessful Imports:")
+        for i in successImports:
+            print("[+] %s" % i)
+        print("\nAlready Existed:")
+        for i in alreadyExists:
+            print("[-] %s" % i)
+        print("\nFailed Imports:")
+        for i in failedImports:
+            print("[!] %s" % i)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--scope")
-parser.add_argument("--blacklist")
-args = parser.parse_args()
-if args.scope:
-    importScope(args.scope, False)
-if args.blacklist:
-    importScope(args.blacklist, True)
+def main():
+    parser_desc = "Server-side utility for populating scope/blacklist from a file directly to the database"
+    parser_epil = "Be sure that you're running this from within the virtual environment for the server."
+    parser = argparse.ArgumentParser(description=parser_desc, epilog=parser_epil)
+    parser.add_argument("--scope", metavar='FILE', help="A file containing line-separated IPs and CIDR ranges to be added to scope.")
+    parser.add_argument("--blacklist", metavar='FILE', help="A file containing line-separated IPs and CIDR ranges to be added to the blacklist.")
+    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Print list of successful, already existing, and failed imports.")
+    args = parser.parse_args()
+
+    if args.scope:
+        importScope(args.scope, False, args.verbose)
+    if args.blacklist:
+        importScope(args.blacklist, True, args.verbose)
+
+if __name__ == "__main__":
+    main()
