6c6cbdaee3112043993ef5955b04ccf5fc807f28
0xdade <0xdade@users.noreply.github.com>
2019-02-09 21:02:33 -0800

    Hover on dates for full timestamp

--- natlas-server/app/filters.py
+++ natlas-server/app/filters.py
@@ -4,5 +4,7 @@ from datetime import datetime
 bp = Blueprint('filters', __name__)
 
 @bp.app_template_filter('ctime')
-def ctime(s):
+def ctime(s, human=False):
+    if human:
+        return datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f').strftime("%Y-%m-%d %H:%M:%S")
     return datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f').strftime("%Y-%m-%d")
