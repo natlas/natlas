34274bf7279d097ccc6dc1b899f4ee4d46c229c2
0xdade <0xdade@users.noreply.github.com>
2019-02-17 00:36:09 -0800

    bugfix agent config loading from file
    All environment variables are strings by default, so added helper methods to return them as ints or bools as necessary. This needs to be patched for v0.5.0, likely causing a v0.5.1 by the end of the weekend. Closes #97.

--- natlas-agent/config.py
+++ natlas-agent/config.py
@@ -6,10 +6,22 @@ class Config:
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     load_dotenv(os.path.join(BASEDIR, '.env'))
 
+    def getInt(self, varname):
+        tmp = os.environ.get(varname)
+        if tmp:
+            return int(tmp)
+        return None
+
+    def getBool(self, varname):
+        tmp = os.environ.get(varname)
+        if tmp and tmp.upper() == "TRUE":
+            return True
+        return None
+
     def __init__(self):
         self.server = os.environ.get('NATLAS_SERVER_ADDRESS') or 'http://127.0.0.1:5000' # url of server to get/submit work to
-        self.max_threads = os.environ.get('NATLAS_MAX_THREADS') or 3 # maximum number of threads to utilize
-        self.scan_local = os.environ.get('NATLAS_SCAN_LOCAL') or False # Are we allowed to scan local addresses?
-        self.request_timeout = os.environ.get('NATLAS_REQUEST_TIMEOUT') or 15 # seconds, default time to wait for the server to respond
-        self.backoff_max = os.environ.get('NATLAS_BACKOFF_MAX') or 300 # seconds, 5 minutes default
-        self.backoff_base = os.environ.get('NATLAS_BACKOFF_BASE') or 1 # seconds
+        self.max_threads = self.getInt('NATLAS_MAX_THREADS') or 3 # maximum number of threads to utilize
+        self.scan_local = self.getBool('NATLAS_SCAN_LOCAL') or False # Are we allowed to scan local addresses?
+        self.request_timeout = self.getInt('NATLAS_REQUEST_TIMEOUT') or 15 # seconds, default time to wait for the server to respond
+        self.backoff_max = self.getInt('NATLAS_BACKOFF_MAX') or 300 # seconds, 5 minutes default
+        self.backoff_base = self.getInt('NATLAS_BACKOFF_BASE') or 1 # seconds
