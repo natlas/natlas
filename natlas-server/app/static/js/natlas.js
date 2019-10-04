c699fa6771ce19f955c183c83579403489913d97
0xdade <0xdade@users.noreply.github.com>
2019-10-04 00:30:20 -0700

    Check for update button + Clickable version string
    Check for update will check against the github api to see if there is a new latest tag and offer a link if there is. The version string in the footer can be clicked to open a new tab taking you to the github page about that particular version release. Closes #48.

--- natlas-server/app/static/js/natlas.js
+++ natlas-server/app/static/js/natlas.js
@@ -8,42 +8,65 @@ $(function() {
 
 var modal_content_loaded = false;
 function loadModalContent() {
-  if (modal_content_loaded){
-    return
-  }
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "/searchmodal")
-  xhr.send();
-  xhr.onload = function() {
-    $('#searchHelpContent').html(xhr.response);
-    modal_content_loaded = true;
-  }
+	if (modal_content_loaded){
+		return
+	}
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", "/searchmodal")
+	xhr.send();
+	xhr.onload = function() {
+		$('#searchHelpContent').html(xhr.response);
+		modal_content_loaded = true;
+	}
 }
 
 $(document).ready(function() {
-    $('.dataTable').DataTable( {
-        "columnDefs": [
-            { "orderable": false, "targets": 'table-controls' }
-        ]
-    });
-    $('[data-toggle="popover"]').popover()
+	var updateURL = "https://api.github.com/repos/natlas/natlas/releases/latest";
+	var latestURL = "https://github.com/natlas/natlas/releases/latest"
+	$('#checkForUpdate').click(function() {
+		var btn = $(this);
+		$.ajax({
+			url: updateURL,
+			type:"GET",
+			success: function(result){
+				if (result.tag_name != $('#natlasVersion').text()) {
+					btn.popover({content: "Update found: <a href=\"" + latestURL + "\">" +result.tag_name+"</a>", html:true}).popover('show');
+				}
+				else {
+					btn.popover({content: "No Updates Found!", trigger:"focus"}).popover('show');
+				}
+			},
+			error: function(error){
+				console.log(error)
+			}
+		});
+	});
+})
+
+$(document).ready(function() {
+		$('.dataTable').DataTable( {
+				"columnDefs": [
+						{ "orderable": false, "targets": 'table-controls' }
+				]
+		});
+		$('[data-toggle="popover"]').popover()
 });
 
 $(document).ready(function() {
 
-  var btn = $('#backtotop');
-  btn.tooltip();
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
+	var btn = $('#backtotop');
+	btn.tooltip();
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
 
-  btn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
-  });
+	btn.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
+	});
 
 });
