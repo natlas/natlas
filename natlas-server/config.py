407375b5df6c86937173770bd8735c8997850014
0xdade <0xdade@users.noreply.github.com>
2019-10-15 20:30:36 -0700

    Preparing configs for v0.6.6

--- natlas-server/config.py
+++ natlas-server/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 class Config(object):
 
 	# Current Version
-	NATLAS_VERSION="0.6.5"
+	NATLAS_VERSION="0.6.6"
 
 	BASEDIR = os.path.abspath(os.path.dirname(__file__))
 	load_dotenv(os.path.join(BASEDIR, '.env'))
