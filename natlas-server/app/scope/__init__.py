72dd5cbbfe42ce5414e5474b555aa1f4a1e91deb
lrwxr-xr-x <ross.snider@gmail.com>
2019-11-25 22:28:21 +0000

    Remove classes which can not be exported from the scope module because they are not imported by the file.

--- natlas-server/app/scope/__init__.py
+++ natlas-server/app/scope/__init__.py
@@ -1,10 +1,5 @@
-
 from app.scope.scope import ScopeManager
-from app.scope.scope import IPScanManager
-from app.scope.scope import CyclicPRNG
 
 __all__ = [
-	'ScopeManager',
-	'CyclicPRNG',
-	'IPScanManager'
+	'ScopeManager'
 ]
