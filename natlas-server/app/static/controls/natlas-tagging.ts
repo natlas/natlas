4b09aba8e381bda8f86a970e48dd6d3cad6081c4
Adam Jacques <adam@technowizardry.net>
2020-04-26 19:14:39 -0700

    Fix CodeClimate issues and add more TypeScript

--- natlas-server/app/static/controls/natlas-tagging.ts
+++ natlas-server/app/static/controls/natlas-tagging.ts
@@ -1,6 +1,12 @@
 import * as $ from 'jquery';
 
-function onModalShow(event) {
+function setModalDetails(modal: any, title: String, action: String, submitLabel: String): void {
+	modal.find('.modal-title').text(title);
+	modal.find('#tagScopeForm').attr('action', action);
+	modal.find('#tagScopeSubmit').text(submitLabel);
+}
+
+function onModalShow(event: JQueryEventObject): void {
 	var button = $(event.relatedTarget); // Button that triggered the modal
 	var scopeid = button.data('scopeid'); // Extract info from data-* attributes
 	var scopetarget = button.data('scopetarget'); // Extract info from data-* attributes
@@ -10,18 +16,14 @@ function onModalShow(event) {
 	var modal = $(this);
 	$("select[name='tagname']").val("");
 	if (tagaction == 'add') {
-		modal.find('.modal-title').text('Add tag to ' + scopetarget);
-		modal.find('#tagScopeForm').attr('action', "/admin/scope/"+scopeid+"/tag");
-		modal.find('#tagScopeSubmit').text('Add Tag');
-		if (tagstr != ''){
-			tags.forEach(item => {
-				$("option[value=\"" + item.trim() + "\"]").attr({'hidden': '', 'disabled': ''}); // hide this option because it's already added
-			});
+		setModalDetails(modal, 'Add tag to ' + scopetarget, "/admin/scope/"+scopeid+"/tag", 'Add Tag');
+		if (tagstr != '') {
+			for (const item of tags) {
+				$("option[value=\"" + item.trim() + "\"]").attr({'hidden': '', 'disabled': ''});
+			}
 		}
 	} else {
-		modal.find('.modal-title').text('Remove tag from ' + scopetarget);
-		modal.find('#tagScopeForm').attr('action', "/admin/scope/"+scopeid+"/untag");
-		modal.find('#tagScopeSubmit').text('Remove Tag');
+		setModalDetails(modal, 'Remove tag from ' + scopetarget, "/admin/scope/" + scopeid + "/untag", 'Remove Tag');
 
 		// Set all options to hidden & disabled by default
 		$("select[name='tagname'] > option").each(function() {
@@ -29,21 +31,21 @@ function onModalShow(event) {
 		});
 
 		// Enable options that are relevant
-		tags.forEach(item => {
+		for (const item of tags) {
 			$("option[value=\"" + item.trim() + "\"]").removeAttr('hidden disabled');
-		});
+		}
 	}
-};
+}
 
-function onModalHide() {
+function onModalHide(): void {
 	// Set all options to enabled and visible by default
 	$("select[name='tagname'] > option").each(function() {
 		$(this).removeAttr('hidden disabled');
 	});
-};
+}
 
-export function registerTagModalEvents() {
+export function registerTagModalEvents(): void {
 	$('#tagmodal').on('show.bs.modal', onModalShow);
 
 	$('#tagmodal').on('hide.bs.modal', onModalHide);
-};
+}
