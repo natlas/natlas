0534f833dae28ad065a41f032b76965556337a81
Adam Jacques <adam@technowizardry.net>
2020-04-26 23:21:47 -0700

    Encapsulate XHR requests

--- natlas-server/app/static/util/version-check.ts
+++ natlas-server/app/static/util/version-check.ts
@@ -1,17 +1,22 @@
+import makeXhrCall from './xhr';
+
 const updateURL = "https://api.github.com/repos/natlas/natlas/releases/latest";
 export const LatestURL = "https://github.com/natlas/natlas/releases/latest";
+
+class GithubReleases {
+	tag_name: string
+};
 
 // Not ideal. We should pack this into the bundle instead
 export function thisVersion(): String {
 	return document.getElementById('natlasVersion').innerText
 }
 
-function extractVersion(result: any) {
+function extractVersion(result: GithubReleases): String {
 	return result.tag_name;
 }
 
 export function getLatestVersion(): Promise<String> {
-	return fetch(updateURL)
-		.then(response => response.json())
+	return makeXhrCall<GithubReleases>(updateURL)
 		.then(extractVersion);
 }
