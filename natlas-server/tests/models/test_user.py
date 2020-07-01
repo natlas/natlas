cb085e5aee75e3641b0c0113a86f2ccca36363e7
0xdade <0xdade@users.noreply.github.com>
2020-06-30 23:09:04 -0700

    User tests and fixes #330

---
+++ natlas-server/tests/models/test_user.py
@@ -0,0 +1,45 @@
+import secrets
+
+import pytest
+
+from app import db
+from app.models import User
+
+test_password = secrets.token_urlsafe(16)
+
+
+def test_new_user(app):
+    test_user = User(email="newuser@example.com", is_active=True)
+    test_user.set_password(test_password)
+    db.session.add(test_user)
+    db.session.commit()
+    assert test_user.check_password(test_password)
+    assert not test_user.check_password("password")
+    assert test_user.is_active
+    assert not test_user.is_admin
+    assert not test_user.password_reset_token
+    assert not test_user.password_reset_expiration
+
+
+def test_validate_email():
+    assert User.validate_email("test@example.com")
+    assert User.validate_email("test+extensions@example.com")
+    assert User.validate_email("test.extensions@example.com")
+    assert not User.validate_email("test")
+    assert not User.validate_email("test@")
+    assert not User.validate_email("test@example")
+
+
+def test_reset_token():
+    email = "resettoken@example.com"
+    user = User(email=email)
+    db.session.add(user)
+    db.session.commit()
+    assert not user.validate_reset_token()
+    assert User.get_reset_token(email)
+    assert user.validate_reset_token()
+    assert User.get_user_by_token(user.password_reset_token)
+    assert not User.get_reset_token("notoken@example.com")
+    assert not User.get_user_by_token("notoken")
+    user.expire_reset_token()
+    assert not user.validate_reset_token()
