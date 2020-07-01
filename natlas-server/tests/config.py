c940ccd1c953dae6aebd547c9e56d44eae029bcc
0xdade <0xdade@users.noreply.github.com>
2020-07-01 15:45:51 -0700

    Tacking on fixes for user invitation and tests

--- natlas-server/tests/config.py
+++ natlas-server/tests/config.py
@@ -3,6 +3,7 @@ from config import Config
 
 class TestConfig(Config):
     MAIL_FROM = "Test Mail <noreply@example.com>"
+    MAIL_SERVER = "localhost"
     TESTING = True
     # This uses an in-memory database
     SQLALCHEMY_DATABASE_URI = "sqlite://"
