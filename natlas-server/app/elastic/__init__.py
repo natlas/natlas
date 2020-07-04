3b6a4cd00310b0adeba0600f4338493a84b86500
0xdade <0xdade@users.noreply.github.com>
2020-07-03 19:05:17 -0700

    Extract indices into their own class

--- natlas-server/app/elastic/__init__.py
+++ natlas-server/app/elastic/__init__.py
@@ -1,3 +1,2 @@
 # importing these here allows for "from app.elastic import ElasticInterface" instead of "from app.elastic.interface import ElasticInterface"
-from app.elastic.client import ElasticClient  # noqa: F401
 from app.elastic.interface import ElasticInterface  # noqa: F401
