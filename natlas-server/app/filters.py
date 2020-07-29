424ed99fbc171475f4c69c0433d5c0321df2c133
0xdade <0xdade@users.noreply.github.com>
2020-07-28 23:50:03 -0700

    Fixed: Rendering of screenshots to not assume .png

--- natlas-server/app/filters.py
+++ natlas-server/app/filters.py
@@ -11,6 +11,8 @@ def ctime(s, human=False):
     return parser.parse(s).strftime("%Y-%m-%d %H:%M")
 
 
-@bp.app_template_filter("hashpath")
-def hashpath(inhash):
-    return f"{inhash[0:2]}/{inhash[2:4]}/{inhash}.png"
+@bp.app_template_filter("get_screenshot_path")
+def get_screenshot_path(inhash: str, service: str = "HTTP"):
+    ext_map = {"HTTP": ".png", "HTTPS": ".png", "VNC": ".jpg"}
+
+    return f"{inhash[0:2]}/{inhash[2:4]}/{inhash}{ext_map[service]}"
