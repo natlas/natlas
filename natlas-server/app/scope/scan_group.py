2d99d2ead1f970fb381e992a6eef2de94414de99
dade <0xdade@users.noreply.github.com>
2020-07-31 21:41:38 -0700

    Changed: Scope Manager format and smarter initialization
    * Better initialization of scope manager, more expandable structure

---
+++ natlas-server/app/scope/scan_group.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from typing import Union
+from flask import current_app
+from .scope_collection import ScopeCollection
+from .scan_manager import IPScanManager
+
+
+class ScanGroup:
+    """
+        A scan group two scope collections:
+        - One for scanning scope
+        - One for blacklisted scope
+    """
+
+    def __init__(self, scope: str, blacklist: str):
+        from app.models.scope_item import ScopeItem
+
+        self.scope = ScopeCollection(ScopeItem.getScope)
+        self.blacklist = ScopeCollection(ScopeItem.getBlacklist)
+        self.effective_size = (self.scope.set - self.blacklist.set).size
+        self.scan_manager = None
+
+    def get_scope_size(self) -> int:
+        return self.scope.size
+
+    def get_blacklist_size(self) -> int:
+        return self.blacklist.size
+
+    def get_effective_size(self) -> int:
+        return self.effective_size
+
+    def get_scan_manager(self) -> Union[None, IPScanManager]:
+        return self.scan_manager
+
+    def get_last_cycle_start(self) -> Union[None, datetime]:
+        if self.scan_manager is None:
+            return None
+        else:
+            return self.scan_manager.rng.cycle_start_time
+
+    def get_completed_cycle_count(self) -> int:
+        if self.scan_manager is None:
+            return 0
+        else:
+            return self.scan_manager.rng.completed_cycle_count
+
+    def update(self):
+        self.scope.update()
+        self.blacklist.update()
+        self.update_scan_manager()
+        self.effective_size = (self.scope.set - self.blacklist.set).size
+
+    def update_scan_manager(self):
+        try:
+            self.scan_manager = IPScanManager(
+                self.scope.set,
+                self.blacklist.set,
+                current_app.config["CONSISTENT_SCAN_CYCLE"],
+            )
+        except Exception as e:
+            if self.scan_manager is None or self.scan_manager.get_total() == 0:
+                errmsg = "Scan manager could not be instantiated because there was no scope configured."
+                current_app.logger.warning(f"{str(datetime.utcnow())} - {errmsg}\n")
+            else:
+                raise e
---
+++ natlas-server/app/scope/scope_collection.py
@@ -0,0 +1,19 @@
+from netaddr import IPNetwork, IPSet
+
+
+class ScopeCollection:
+    list = []
+    set = IPSet()
+    size = 0
+
+    def __init__(self, scope_source: callable):
+        """
+            name: A name for this collection of scopes
+            scope_source: A callable that returns a collection of ScopeItem
+        """
+        self.scope_source = scope_source
+
+    def update(self):
+        self.list = [IPNetwork(item.target, False) for item in self.scope_source()]
+        self.set = IPSet(self.list)
+        self.size = self.set.size
