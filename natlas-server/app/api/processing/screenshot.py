05a9d52abe4c4aa98c184387a2726509ba9a1d96
dade <0xdade@users.noreply.github.com>
2020-08-15 08:30:53 -0700

    Fix: Image verification on agent and server (#422)
    * Fix: Image verification on agent and server
    
    This closes #412 by implementing agent side image validation before including the image in the scan results. The server also makes use of a similar function to validate that an image is valid before saving it.

--- natlas-server/app/api/processing/screenshot.py
+++ natlas-server/app/api/processing/screenshot.py
@@ -2,7 +2,33 @@ from flask import current_app
 import base64
 import hashlib
 import os
-from PIL import Image
+from io import BytesIO
+from PIL import Image, UnidentifiedImageError
+
+
+def is_valid_image(path: str) -> bool:
+    try:
+        Image.open(path)
+        return True
+    except (FileNotFoundError, UnidentifiedImageError):
+        return False
+
+
+def get_file_ext(service: str) -> str:
+    service_ext_map = {
+        "VNC": ".jpg",
+        "HTTP": ".png",
+        "HTTPS": ".png",
+        "default": ".png",
+    }
+    return service_ext_map.get(service, "default")
+
+
+def get_file_path(img_hash: str, subdir: str, ext: str) -> str:
+    hash_path = f"{img_hash[0:2]}/{img_hash[2:4]}"
+    hash_dir = os.path.join(current_app.config["MEDIA_DIRECTORY"], subdir, hash_path)
+    os.makedirs(hash_dir, exist_ok=True)
+    return os.path.join(hash_dir, img_hash + ext)
 
 
 def create_thumbnail(fname, file_ext):
@@ -10,46 +36,41 @@ def create_thumbnail(fname, file_ext):
     thumb = Image.open(fname)
     thumb.thumbnail(thumb_size)
     thumb_hash = hashlib.sha256(thumb.tobytes()).hexdigest()
-    thumbhashpath = f"{thumb_hash[0:2]}/{thumb_hash[2:4]}"
-    thumbpath = os.path.join(
-        current_app.config["MEDIA_DIRECTORY"], "thumbs", thumbhashpath
-    )
-    # makedirs attempts to make every directory necessary to get to the "thumbs" folder
-    os.makedirs(thumbpath, exist_ok=True)
-    fname = os.path.join(thumbpath, thumb_hash + file_ext)
+    fname = get_file_path(thumb_hash, "thumbs", file_ext)
     thumb.save(fname)
     thumb.close()
     return thumb_hash
 
 
-def process_screenshots(screenshots):
-    # Handle screenshots
+def process_screenshot(screenshot: dict):
+    file_ext = get_file_ext(screenshot["service"])
+    image = base64.b64decode(screenshot["data"])
+    del screenshot["data"]
 
-    num_screenshots = 0
+    if not is_valid_image(BytesIO(image)):
+        return {}
+
+    image_hash = hashlib.sha256(image).hexdigest()
+    fname = get_file_path(image_hash, "original", file_ext)
+
+    with open(fname, "wb") as f:
+        f.write(image)
+
+    screenshot["hash"] = image_hash
+    screenshot["thumb_hash"] = create_thumbnail(fname, file_ext)
+
+    return screenshot
+
+
+def process_screenshots(screenshots: list) -> tuple:
+    processed_screenshots = []
     for item in screenshots:
-        if item["service"] == "VNC":
-            file_ext = ".jpg"
-        else:  # Handles http, https files from aquatone/chromium-headless
-            file_ext = ".png"
+        screenshot = process_screenshot(item)
+        if not screenshot:
+            current_app.logger.warning(
+                f"Received invalid image for {screenshot['hostname']} port {screenshot['port']}"
+            )
+            continue
+        processed_screenshots.append(item)
 
-        image = base64.b64decode(item["data"])
-        image_hash = hashlib.sha256(image).hexdigest()
-
-        hashpath = f"{image_hash[0:2]}/{image_hash[2:4]}"
-        dirpath = os.path.join(
-            current_app.config["MEDIA_DIRECTORY"], "original", hashpath
-        )
-
-        # makedirs attempts to make every directory necessary to get to the "original" folder
-        os.makedirs(dirpath, exist_ok=True)
-
-        fname = os.path.join(dirpath, image_hash + file_ext)
-        with open(fname, "wb") as f:
-            f.write(image)
-        item["hash"] = image_hash
-        del item["data"]
-
-        item["thumb_hash"] = create_thumbnail(fname, file_ext)
-        num_screenshots += 1
-
-    return screenshots, num_screenshots
+    return processed_screenshots, len(processed_screenshots)
