31b905d7ad40baf6eeb6cad135e51c0cb3091f37
0xdade <0xdade@users.noreply.github.com>
2020-08-19 19:32:18 -0700

    Fixed: Bubble Elasticsearch request error up
    Closes #444 by handling elasticsearch.BadRequest exceptions with a 400 error instead of 500.

--- natlas-server/app/errors/__init__.py
+++ natlas-server/app/errors/__init__.py
@@ -3,3 +3,6 @@ from flask import Blueprint
 bp = Blueprint("errors", __name__)
 
 from app.errors import handlers  # noqa: F401
+from app.errors.errors import NatlasServiceError, NatlasSearchError
+
+__all__ = ["handlers", "NatlasServiceError", "NatlasSearchError"]
