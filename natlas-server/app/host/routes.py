f37da70cd7143264b82c9077e5e9ee7f7da9c436
0xdade <0xdade@users.noreply.github.com>
2020-08-22 22:36:22 -0700

    Fix: Validate IP addresses in app routes via custom converter
    Fixes #449 by introducing a process for creating custom werkzeug map converters for custom types.

--- natlas-server/app/host/routes.py
+++ natlas-server/app/host/routes.py
@@ -21,8 +21,7 @@ from app.auth.wrappers import is_authenticated
 from app import db
 
 
-@bp.route("/<ip>")
-@bp.route("/<ip>/")
+@bp.route("/<ip:ip>")
 @is_authenticated
 def host(ip):
     info, context = hostinfo(ip)
@@ -43,8 +42,7 @@ def host(ip):
     )
 
 
-@bp.route("/<ip>/history")
-@bp.route("/<ip>/history/")
+@bp.route("/<ip:ip>/history")
 @is_authenticated
 def host_history(ip):
     info, context = hostinfo(ip)
@@ -81,7 +79,7 @@ def host_history(ip):
     )
 
 
-@bp.route("/<ip>/<scan_id>")
+@bp.route("/<ip:ip>/<scan_id>")
 @is_authenticated
 def host_historical_result(ip, scan_id):
     delForm = DeleteForm()
@@ -103,7 +101,7 @@ def host_historical_result(ip, scan_id):
     )
 
 
-@bp.route("/<ip>/<scan_id>.<ext>")
+@bp.route("/<ip:ip>/<scan_id>.<ext>")
 @is_authenticated
 def export_scan(ip, scan_id, ext):
     if ext not in ["xml", "nmap", "gnmap", "json"]:
@@ -121,8 +119,7 @@ def export_scan(ip, scan_id, ext):
         return abort(404)
 
 
-@bp.route("/<ip>/screenshots")
-@bp.route("/<ip>/screenshots/")
+@bp.route("/<ip:ip>/screenshots")
 @is_authenticated
 def host_screenshots(ip):
     page = int(request.args.get("p", 1))
@@ -157,7 +154,7 @@ def host_screenshots(ip):
     )
 
 
-@bp.route("/<ip>/rescan", methods=["POST"])
+@bp.route("/<ip:ip>/rescan", methods=["POST"])
 @login_required
 def rescan_host(ip):
     rescanForm = RescanForm()
@@ -201,7 +198,6 @@ def rescan_host(ip):
 
 
 @bp.route("/random")
-@bp.route("/random/")
 @is_authenticated
 def random_host():
     random_host = current_app.elastic.random_host()
---
+++ natlas-server/tests/test_url_converters.py
@@ -0,0 +1,30 @@
+from flask import Flask, abort
+import pytest
+
+from app.url_converters import register_converters
+
+TEST_IP_ADDRS = ["127.0.0.1", "::1"]
+
+
+@pytest.fixture
+def converter_client():
+    app = Flask(__name__)
+    register_converters(app)
+
+    @app.route("/ip/<ip:ip>")
+    def ip_route(ip):
+        if ip in TEST_IP_ADDRS:
+            return ip
+        # abort 400 to differentiate from an invalid ip 404 status code
+        return abort(400)
+
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
+
+
+def test_ip_converter(converter_client):
+    assert converter_client.get("/ip/127.0.0.1").status_code == 200
+    assert converter_client.get("/ip/::1").status_code == 200
+    assert converter_client.get("/ip/254.254.254.254").status_code == 400
+    assert converter_client.get("/ip/127.0.0.1asdf").status_code == 404
