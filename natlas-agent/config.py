3a1cb4ec21a93d510b3de00f0d6a7f9649f13083
dade <0xdade@users.noreply.github.com>
2025-01-01 23:08:17 -0800

    Switch to Pydantic for agent config (#1203)
    * Switch to Pydantic for agent config
    
    * Use env_prefix to reduce duplication where possible

--- natlas-agent/config.py
+++ natlas-agent/config.py
@@ -1,22 +1,22 @@
 import os
+from typing import Self
 
 from dotenv import load_dotenv
+from pydantic import Field, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config:
+class LegacyConfig:
     # Current Version
     NATLAS_VERSION = "0.6.12"
 
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     load_dotenv(os.path.join(BASEDIR, ".env"))
 
-    def get_int(self, varname):  # type: ignore[no-untyped-def]
-        tmp = os.environ.get(varname)
-        if tmp:
-            return int(tmp)
-        return None
+    def get_int(self, varname: str) -> int | None:
+        return int(tmp) if (tmp := os.environ.get(varname)) else None
 
-    def get_bool(self, varname):  # type: ignore[no-untyped-def]
+    def get_bool(self, varname: str) -> bool | None:
         tmp = os.environ.get(varname)
         if tmp and tmp.upper() == "TRUE":
             return True
@@ -75,3 +75,29 @@ class Config:
 
         if self.version_override:
             self.NATLAS_VERSION = self.version_override
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="natlas_")
+    NATLAS_VERSION: str = "0.6.12"
+    base_dir: str = os.path.abspath(os.path.dirname(__file__))
+    server: str = Field(alias="natlas_server_address", default="http://127.0.0.1:5000")
+    data_dir: str = Field(default="/data")
+    ignore_ssl_warn: bool = Field(default=False)
+    max_threads: int = Field(default=3)
+    scan_local: bool = Field(default=False)
+    request_timeout: int = Field(default=15)
+    backoff_max: int = Field(default=300)
+    backoff_base: int = Field(default=1)
+    max_retries: int = Field(default=10)
+    agent_id: str = Field(default="anonymous")
+    auth_token: str | None = Field(alias="natlas_agent_token", default=None)
+    save_fails: bool = Field(default=False)
+    version_override: str | None = Field(default=None)
+    sentry_dsn: str | None = Field(alias="sentry_dsn", default=None)
+
+    @model_validator(mode="after")
+    def override_version(self) -> Self:
+        if self.version_override:
+            self.NATLAS_VERSION = self.version_override
+        return self
